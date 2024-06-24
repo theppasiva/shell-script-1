91318669f2d94ca0ecc7716d698668bccbf05cfd
theppasiva <theppasiva@gmail.com>
2024-06-24 18:02:15 +0530

    weclome.txt

--- 01-shell-script.sh
+++ 01-shell-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
- echo "Ramesh:: ,How are you"
- echo "Suresh::,I am fine ramesh"
- echo "Suresh::, are you going to office"
- echo "Ramesh:: ,yes I am going to office "
+
+PERSON1=Ramesh
+PERSON2=Suresh
+ echo "$PERSON1:: ,How are you $PERSON2"
+ echo "$PERSON2::,I am fine $PERSON1"
+ echo "Suresh::, are you going to office ramesh"
+ echo "Ramesh:: ,yes I am going to office suresh"
