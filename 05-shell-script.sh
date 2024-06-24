0511829188cc1c9f71864b093bdaecbf43c5b231
theppasiva <theppasiva@gmail.com>
2024-06-24 22:38:55 +0530

    shellscript

--- 05-shell-script.sh
+++ 05-shell-script.sh
@@ -1,5 +1,6 @@
 #!bin/bash
-FRUITS=("apple""banana""mango")
+FRUITS=("apple" "banana" "mango")
 echo "First value: ${FRUITS[0]}"
 echo "Second value: ${FRUITS[1]}"
 echo "Third value: ${FRUITS[2]}"
+echo "All the fruits: ${FRUITS[@]}"
