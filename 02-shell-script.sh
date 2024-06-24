495b22d7988a28305df7847eb9474ab642c8d656
theppasiva <theppasiva@gmail.com>
2024-06-24 18:11:14 +0530

    shellscript

--- 02-shell-script.sh
+++ 02-shell-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PERSON1=$1
 PERSON2=$2
+DATE=$(date)
+echo "Today is :$DATE"
  echo "$PERSON1:: ,How are you $PERSON2"
  echo "$PERSON2::,I am fine $PERSON1"
  echo "$PERSON2::, are you going to office $PERSON1"
