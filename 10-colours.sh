27cb700650eed43af2d8da932c1dcb51c6b50387
theppasiva <theppasiva@gmail.com>
2024-06-25 01:09:47 +0530

    shellscript

--- 10-colours.sh
+++ 10-colours.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "Script started excuting at $TIMESTAMP" &>> LOGFILE 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Error:: $2..  is failed"
+        echo -e "Error:: $2..  $R is failed $N"
         exit 1
     else
-        echo "$2  is success"
+        echo -e "$2  $G is success $N"
     fi
 
 }
 if [ $ID -ne 0 ]
 then
-    echo "Error:: please run with root user"
+    echo -e "$R Error:: please run with root user $N"
     exit 1
 else
     echo "You are root user"
