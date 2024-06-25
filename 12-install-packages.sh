5051a2f21e2b623286b885fca4a0ebe1b8dcc93d
theppasiva <theppasiva@gmail.com>
2024-06-25 19:17:54 +0530

    shell-script

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started excuting at $TIMESTAMP" &>> LOGFILE 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Error:: $2..  $R is failed $N"
+        exit 1
+    else
+        echo -e "$2  $G is success $N"
+    fi
+
+}
 
 if [ $ID -ne 0 ]
 then
@@ -12,5 +27,18 @@ then
 else
     echo "You are root user"
 fi
+#echo "All the arguments passed: $@"
+#git mysql postfix net-tools
+#package-git for first time
 
-echo "All the arguments passed: $@"
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $Package"
+    else
+        echo -e "$package is already installed.. $Y Skipping $N"
+    fi
+done
