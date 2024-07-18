140730dbcb578935d5c7cc502f32dfa94b057365
theppasiva <theppasiva@gmail.com>
2024-07-18 23:03:42 +0530

    shellscript

--- ifs.sh
+++ ifs.sh
@@ -10,7 +10,7 @@ then
     echo -e " $R SourceDirectory: $FILE does not exists.$N"
 fi
 
-while IFS=":" read -r username password user_id group_id user_fullname home_dir shell-path
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
     echo "username: $username"
     echo "user-id: $user_id"
