9574390c9ee69bb2a621f59061bb8d5e61667ee3
theppasiva <theppasiva@gmail.com>
2024-07-18 22:15:19 +0530

    shellscript

---
+++ 13-delete-old-logs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SOURCE_DIR="/tmp/shell-script-logs"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [  -d $SOURCE_DIR ]
+then
+    echo  -e "$R source directory: $SOURCE_DIR does not exist. $N"
+fi
