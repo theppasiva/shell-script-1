900fdad1ad50d15d62fe7c716f130ac1b7a92353
theppasiva <theppasiva@gmail.com>
2024-07-19 00:45:59 +0530

    shellscript

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -7,7 +7,7 @@ while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-    if [ $usage -ge $DISK_THRESHOLD]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message="High Disk usage on $partition: $usage"
     fi
