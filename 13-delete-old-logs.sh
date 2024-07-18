dc76ff3bf5dc52eaf5622f314ca3737d90af242a
theppasiva <theppasiva@gmail.com>
2024-07-18 22:30:58 +0530

    shellscript

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -9,3 +9,10 @@ if [ ! -d $SOURCE_DIR ]
 then
     echo  -e "$R source directory: $SOURCE_DIR does not exist. $N"
 fi
+
+FILES_TO_DELETE=$( find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting files: $line"
+done <<< $FILES_TO_DELETE
