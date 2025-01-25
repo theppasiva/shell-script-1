e83c81dcd81099ad3669d26077f4856c35f04f22
theppasiva <theppasiva@gmail.com>
2025-01-25 21:04:04 +0530

    updated shellscript

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 PERSON1=ROBERT
 PERSON2=RAHEEM
- echo "$PERSON1:: ,How are you $PERSON2"
- echo "$PERSON2::,I am fine $PERSON1"
- echo "$PERSON2::, are you going to office $PERSON1"
- echo "$PERSON1:: ,yes I am going to office $PERSON2"
+
+echo "$PERSON1:: ,How are you $PERSON2"
+echo "$PERSON2::,I am fine $PERSON1"
+echo "$PERSON2::, are you going to office $PERSON1"
+echo "$PERSON1:: ,yes I am going to office $PERSON2"
