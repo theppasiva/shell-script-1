39dccda47a4247048b973dbaf99edf47dfd9f144
theppasiva <theppasiva@gmail.com>
2025-01-25 23:55:43 +0530

    updated shellscript

--- 05-Arrays.sh
+++ 05-Arrays.sh
@@ -1,7 +1,14 @@
 #!bin/bash
-#FRUITS=("apple" "banana" "mango")
-FRUITS=(1 2 3)
-echo "First value: ${FRUITS[0]}"
-echo "Second value: ${FRUITS[1]}"
-echo "Third value: ${FRUITS[2]}"
-echo "All the fruits: ${FRUITS[@]}"
+# #FRUITS=("apple" "banana" "mango")
+# FRUITS=(1 2 3)
+# echo "First value: ${FRUITS[0]}"
+# echo "Second value: ${FRUITS[1]}"
+# echo "Third value: ${FRUITS[2]}"
+# echo "All the fruits: ${FRUITS[@]}"
+echo "please enter today"
+read TODAY
+if [$TODAY!="sunday"]
+then
+echo "go to school"
+else
+echo "happy holiday"
