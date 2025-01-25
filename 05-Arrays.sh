2fae315fdf3e816db74fe9671b14654f025263b9
theppasiva <theppasiva@gmail.com>
2025-01-25 23:30:11 +0530

    updated shellscript

--- 05-Arrays.sh
+++ 05-Arrays.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-FRUITS=("apple","banana","mango")
+FRUITS=("apple" "banana" "mango")
 echo "First value: ${FRUITS[0]}"
 echo "Second value: ${FRUITS[1]}"
 echo "Third value: ${FRUITS[2]}"
