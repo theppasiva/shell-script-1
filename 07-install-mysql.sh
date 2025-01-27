e436dccc03d009a554bc955318a84b0305cc3cb7
theppasiva <theppasiva@gmail.com>
2025-01-27 09:39:49 +0530

    updated shell script

--- 07-install-mysql.sh
+++ 07-install-mysql.sh
@@ -1,29 +1,49 @@
+# #!/bin/bash
+# ID=$(id -u)
+# if [ $ID -ne 0 ]
+# then
+#     echo "Error:: please run with root user"
+#     exit 2
+# else
+#     echo "You are root user"
+# fi
+
+# yum install mysql -y
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Error:: Installing mysql is failed"
+#     exit 1
+# else
+#     echo "Installing mysql is success"
+# fi
+
+# yum install git -y
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Error:: Installing git is failed"
+#     exit 1
+# else
+#     echo "Installing git is success"
+# fi
+
 #!/bin/bash
 ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "Error:: please run with root user"
-    exit 1
+    #exit 2
 else
     echo "You are root user"
 fi
 
 yum install mysql -y
 
-if [ $? -ne 0 ]
-then
-    echo "Error:: Installing mysql is failed"
-    exit 1
-else
-    echo "Installing mysql is success"
-fi
-
-yum install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "Error:: Installing git is failed"
-    exit 1
-else
-    echo "Installing git is success"
-fi
+# if [ $? -ne 0 ]
+# then
+#     echo "Error:: Installing mysql is failed"
+#     exit 1
+# else
+#     echo "Installing mysql is success"
+# fi
