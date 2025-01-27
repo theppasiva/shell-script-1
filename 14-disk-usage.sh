6f36ab8765ac88441fc5668a022f526b714aa610
theppasiva <theppasiva@gmail.com>
2025-01-27 21:16:48 +0530

    updated diskusage.sh

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -11,11 +11,11 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage <br>"
+        message+="High Disk Usage on $partition: $usage \n"
     fi
 done <<< $DISK_USAGE
 
-echo -e "Message: $message/n"
+echo -e "Message: $message"
 
-#echo "$message" | mail -s "High Disk Usage" theppasiva@gmail.com
+echo "$message" | mail -s "High Disk Usage" theppasiva@gmail.com
 #sh mail.sh "DevOps Team" "High Disk Usage" "$message" "theppasiva@gmail.com" "ALERT High Disk Usage"
