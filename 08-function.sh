7476ec2a143892bbdc24f7f4843d1b82c628a458
theppasiva <theppasiva@gmail.com>
2025-01-27 10:13:39 +0530

    updated shell script

--- 08-function.sh
+++ 08-function.sh
@@ -1,41 +1,14 @@
-# #!/bin/bash
-# ID=$(id -u)
-# echo "script name: $0"
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo "Error:: $2..  is failed"
-#         exit 1
-#     else
-#         echo "$2  is success"
-#     fi
-
-# }
-# if [ $ID -ne 0 ]
-# then
-#     echo "Error:: please run with root user"
-#     exit 1
-# else
-#     echo "You are root user"
-# fi
-
-# yum install mysql -y
-# VALIDATE $? "Installing MYSQL"
-
-# yum install git -y
-# VALIDATE $? "Installing Git"
-
 #!/bin/bash
 ID=$(id -u)
 echo "script name: $0"
 VALIDATE(){
-if [ $? -ne 0 ]
-then
-    echo "Error:: Installing mysql is failed"
-    exit 1
-else
-    echo "Installing mysql is success"
-fi
+    if [ $1 -ne 0 ]
+    then
+        echo "Error:: $2..  is failed"
+        exit 1
+    else
+        echo "$2  is success"
+    fi
 
 }
 if [ $ID -ne 0 ]
@@ -47,7 +20,34 @@ else
 fi
 
 yum install mysql -y
-VALIDATE 
+VALIDATE $? "Installing MYSQL"
 
 yum install git -y
-VALIDATE 
+VALIDATE $? "Installing Git"
+
+# #!/bin/bash
+# ID=$(id -u)
+# echo "script name: $0"
+# VALIDATE(){
+# if [ $? -ne 0 ]
+# then
+#     echo "Error:: Installing  is failed"
+#     exit 1
+# else
+#     echo "Installing  is success"
+# fi
+
+# }
+# if [ $ID -ne 0 ]
+# then
+#     echo "Error:: please run with root user"
+#     exit 1
+# else
+#     echo "You are root user"
+# fi
+
+# yum install mysql -y
+# VALIDATE 
+
+# yum install git -y
+# VALIDATE 
